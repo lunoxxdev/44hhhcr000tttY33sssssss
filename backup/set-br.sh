19b4348e2cfc139b9c604437ceab4470158c2860
lunoxxdev <admin@lunoxx.dev>
2023-06-27 00:35:02 +0700

    Ngentot Lu

--- backup/set-br.sh
+++ backup/set-br.sh
@@ -53,8 +53,8 @@ red='\e[1;31m'
 green='\e[0;32m'
 yell='\e[1;33m'
 NC='\e[0m'
-echo "Setting UP"
-echo "Progress..."
+echo "Sabar Cok !!!!"
+echo "Loading Asui.."
 sleep 3
 echo ""
 #green() { echo -e "\\033[32;1m${*}\\033[0m"; }
